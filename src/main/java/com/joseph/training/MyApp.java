30fd74141945f4dcd520462a7961b08f4825d779
Tamal2017 <tingangt@gmail.com>
2022-02-27 20:02:57 -0500

    Configure context using ClassPathXml

--- src/main/java/com/joseph/training/MyApp.java
+++ src/main/java/com/joseph/training/MyApp.java
@@ -1,12 +1,12 @@
 package com.joseph.training;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 
   public static void main(String[] args) {
-    ApplicationContext ctx = new FileSystemXmlApplicationContext("application-context.xml");
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
     var service = ctx.getBean(MyService.class);
     service.doSomething();
   }
