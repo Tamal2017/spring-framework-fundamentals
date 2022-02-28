28a5b6f18b31f48531f0390088508071e174d580
Tamal2017 <tingangt@gmail.com>
2022-02-28 17:45:15 -0500

    Manage beans: DI by settter

--- src/main/java/com/joseph/training/MyService.java
+++ src/main/java/com/joseph/training/MyService.java
@@ -4,12 +4,12 @@ public class MyService {
 
   private MyRepository repository;
 
-  public MyService(MyRepository repository) {
-    this.repository = repository;
-  }
-
   public void doSomething() {
     System.out.println("Doing some business logic !");
     repository.doQuery();
+  }
+
+  public void setRepository(MyRepository repository) {
+    this.repository = repository;
   }
 }
